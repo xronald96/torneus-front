643dcaa3780bf630f5b9e5a4a5af5d24cbe41009
ronald <ronald881996@gmail.com>
2023-03-31 18:28:07 +0200

    new

--- src/components/Tournmanet.tsx
+++ src/components/Tournmanet.tsx
@@ -8,31 +8,33 @@ export default function Tournament({ data }: { data: any[] }) {
 			<div className='pb-2 flex mb-3 font-poppins font-bold border-main border-b-4 w-36'>
 				Torneos Activos
 			</div>
-			{data.map((tournament) => {
-				return (
-					<Link
-						href={`/tournament/${tournament._id}`}
-						className='border-2 w-52 rounded-md p-5 no-underline font-poppins font-semibold text-main hover:text-main'
-						key={tournament.name}
-					>
-						<div className='no-underline font-poppins font-semibold from-neutral-900'>
-							<Image
-								className='w-full'
-								alt=''
-								src={torneysIcon}
-								width={50}
-								height={70}
-							/>
-							<div className='text-bold no-underline font-poppins font-semibold'>
-								{tournament.name}
+			<div className='flex flex-wrap border-main-2'>
+				{data.map((tournament) => {
+					return (
+						<Link
+							href={`/tournament/${tournament._id}`}
+							className='border-2 w-52 rounded-md p-5 no-underline font-poppins font-semibold text-main hover:text-main mr-5 mb-5'
+							key={tournament.name}
+						>
+							<div className='no-underline font-poppins font-semibold from-neutral-900'>
+								<Image
+									className='w-full'
+									alt=''
+									src={torneysIcon}
+									width={50}
+									height={70}
+								/>
+								<div className='text-bold no-underline font-poppins font-semibold'>
+									{tournament.name}
+								</div>
+								<div className='no-underline font-poppins from-stone-400'>
+									{tournament.date}
+								</div>
 							</div>
-							<div className='no-underline font-poppins from-stone-400'>
-								{tournament.date}
-							</div>
-						</div>
-					</Link>
-				);
-			})}
+						</Link>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
